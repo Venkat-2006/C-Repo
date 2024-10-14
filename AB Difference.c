c98b8c13e79c8fc7b9e0f71cc7f54dd557fd5c58
Venkat-2006 <23a91a0549@aec.edu.in>
2024-10-14 11:25:37 +0530

    Uploading code: AB Difference.c

---
+++ AB Difference.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int A, B;
+        scanf("%d %d", &A, &B);
+            
+                int sum = A + B;
+                    int product = A * B;
+                        
+                            int difference = abs(sum - product);
+                                
+                                    printf("%d\n", difference);
+                                        
+                                            return 0;
+                                            }
+                                            
