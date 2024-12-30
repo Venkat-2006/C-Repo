68cf45e6e7d129fc83ce007aaa75e720053eb8ea
Venkat-2006 <23a91a0549@aec.edu.in>
2024-12-30 14:23:12 +0530

    Uploading code: Find the Digits Count.c

---
+++ Find the Digits Count.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int N, count = 0;
+    scanf("%d", &N);
+    while (N > 0) {
+        N = N / 10;  
+        count++;  
+    }
+    printf("%d\n", count);
+    return 0;
+}
